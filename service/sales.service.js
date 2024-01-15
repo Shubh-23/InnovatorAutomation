5e568a32a17a9a40e4b8e02d8afe5d1aaf6b9881
shubham <potterparker09@gmail.com>
2024-01-15 22:36:40 +0530

    change done by shubham

--- service/sales.service.js
+++ service/sales.service.js
@@ -4,67 +4,63 @@ const moment = require('moment')
 
 class Products {
     async inserReportDetailsByEmployeeId(params) {
-        try{
-        var loginTime = new Date(params.login_time)
-        var logoutTime = new Date(params.logout_time)
-        const data = {
-            "employee_id": params.employee_id,
-            "stick_count": params.stick_count,
-            "reset_count": params.reset_count,
-            "login_time": loginTime,
-            "logout_time": logoutTime,
-        }
+        
         var responseData = []
-        console.log(data);
-
-            const employeeData = new employeeReportTable(data);
-
-            // Save the user data to the database using async/await
-            const savedEmployeeData = await employeeData.save();
-  
-        // console.log('User object with hashed password:', savedEmployeeData);
-  
-        return savedEmployeeData;
-      } catch (error) {
-        console.error('Error:', error);
-        throw error; // Rethrow the error or handle it accordingly
-      }
-
-        // return await salesTable.forge().query((qb) => {
-        //     qb.where({ "Date": date })
-        //     qb.andWhere({ "user_id": params.userId })
-        // }).fetchAll().then((result) => {
-
-        //     if (result.length == 0) {
-        //         const salesdata = new salesTable(data)
-        //         return salesdata.save(null).tap(res => {
-        //             return res
-        //         }).catch((err) => {
-        //             return err
-        //         })
-
-        //     } else {
-        //       return  promise.all(result.models).each((res) => {
-        //             console.log(res.get('user_id'))
-        //             return salesTable.forge().query((qb) => {
-        //                 qb.where({ "user_id": res.get('user_id') })
-        //                 qb.andWhere({ "Date": date })
-        //             }).fetch().then((response) => {
-        //                 response.save(data, { patch: true }).then((updateData) => {
-        //                     console.log(updateData);
-        //                     responseData.push(updateData)
-        //                     return responseData
-        //                 })
-        //                 return responseData
-        //             })
-        //         }).then((res)=>{
-        //             return res
-        //         })
-
-        //     }
-        // }).catch((err) => {
-        //     return err
-        // })
+        try {
+            const loginTime = new Date(params.login_time);
+            let logoutTime;
+        
+            if (params.logout_time !== null) {
+                logoutTime = new Date(params.logout_time);
+            } else {
+                logoutTime = params.logout_time;
+            }
+        
+            const data = {
+                "employee_id": params.employee_id,
+                "stick_count": params.stick_count,
+                "reset_count": params.reset_count,
+                "login_time": loginTime,
+                "logout_time": logoutTime,
+            };
+        
+            const existingRecords = await employeeReportTable.forge()
+                .query((qb) => {
+                    qb.where({ "login_time": loginTime })
+                        .andWhere({ "employee_id": params.employee_id });
+                })
+                .fetchAll();
+        
+            if (existingRecords.length === 0) {
+                const employeedata = new employeeReportTable(data);
+                const savedRecord = await employeedata.save(null);
+                return savedRecord;
+            } else {
+                const updatedRecords = await Promise.all(existingRecords.models.map(async (res) => {
+                    console.log(res.get('employee_id'));
+        
+                    const response = await employeeReportTable.forge()
+                        .query((qb) => {
+                            qb.where({ "employee_id": res.get('employee_id') })
+                                .andWhere({ "login_time": loginTime });
+                        })
+                        .fetch();
+        
+                    if (response) {
+                        const updateData = await response.save(data, { patch: true });
+                        console.log(new Date(updateData.attributes.login_time));
+                        responseData.push(updateData);
+                        return responseData;
+                    }
+                }));
+        
+                return updatedRecords;
+            }
+        } catch (err) {
+            console.error(err);
+            throw err; // Rethrow the error to handle it at a higher level if needed
+        }
+        
 
 
     }
@@ -106,19 +102,19 @@ class Products {
 
 
     getLastSevenDays(params) {
-        var  dateTo
+        var dateTo
         var dateFrom
         console.log(params);
         if (params.length == 0) {
-              dateTo = moment().format('YYYY-MM-DD');
-             dateFrom = moment().subtract(7,'d').format('YYYY-MM-DD');
-            }else{
             dateTo = moment().format('YYYY-MM-DD');
-           dateFrom = moment().subtract(+params.dateCount,`${params.dateType}`).format('YYYY-MM-DD');
+            dateFrom = moment().subtract(7, 'd').format('YYYY-MM-DD');
+        } else {
+            dateTo = moment().format('YYYY-MM-DD');
+            dateFrom = moment().subtract(+params.dateCount, `${params.dateType}`).format('YYYY-MM-DD');
         }
-            console.log(dateTo,dateFrom);
-            return salesTable.forge().query((qb) => {
-            qb.whereBetween( "Date", [dateFrom, dateTo] )      
+        console.log(dateTo, dateFrom);
+        return salesTable.forge().query((qb) => {
+            qb.whereBetween("Date", [dateFrom, dateTo])
         }).fetchAll().then((data) => {
             // console.log(data);
             return data
