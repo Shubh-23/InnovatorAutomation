7a3a76caede5840402d227c715138b7c8f706c8b
shubham <potterparker09@gmail.com>
2024-01-15 22:37:18 +0530

    change done

--- database/knexfile.js
+++ database/knexfile.js
@@ -4,17 +4,17 @@ module.exports = {
 
   development: {
     client: 'mysql',
-    // connection: {
-    //   host: 'sql11.freesqldatabase.com',
-    //   database: 'sql11676677',
-    //   user: 'sql11676677',
-    //   password: 'lmKPF8lELX'
-    // },
     connection: {
-      database: 'employeeDB',
-      user:     'root',
-      password: 'root'
+      host: 'sql11.freesqldatabase.com',
+      database: 'sql11676677',
+      user: 'sql11676677',
+      password: 'lmKPF8lELX'
     },
+    // connection: {
+    //   database: 'employeeDB',
+    //   user:     'root',
+    //   password: 'root'
+    // },
     pool: {
       min: 2,
       max: 10
